e5d6d732df73c7aa7e256f2bde65be17eb5714c5
Douglas Luman <dluman@douglasjluman.com>
2020-03-21 18:45:03 -0400

    Get an A+

--- src/main/java/practicaleight/GumballMachine.java
+++ src/main/java/practicaleight/GumballMachine.java
@@ -13,17 +13,21 @@ public class GumballMachine {
    * @param args The command line arguments
    */
   public static void main(String[] args) {
+    // Create Scanner to take input
     Scanner input = new Scanner(System.in);
-    
+    // Prompt user for choice
     System.out.print("Would you like a gumball (y/n)?");
     String choice = input.nextLine().toUpperCase();
-    
+    // Create switch statement to handle choice
     switch (choice) {
       case "Y":
+        // If "Y," create a new gumball
         Gumball gumball = new Gumball();
+        // Print out the color
         System.out.print("You received a " + gumball + " one!");
         break;
       default:
+        // Otherwise, it's too bad, so sad
         System.out.print("Oh, that's too bad.");
         break;
     }
